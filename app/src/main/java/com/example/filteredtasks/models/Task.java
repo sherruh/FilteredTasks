3dd5890d520703fea620c7239436d6b9544895ce
sherruh <sherruh@gmail.com>
2019-03-06 19:10:53 +0600

    Update  model

--- app/src/main/java/com/example/filteredtasks/models/Task.java
+++ app/src/main/java/com/example/filteredtasks/models/Task.java
@@ -1,22 +1,51 @@
 package com.example.filteredtasks.models;
 
+import enums.ProblemType;
+import enums.Region;
+import enums.Technology;
+
 public class Task {
     private boolean inWork;
     private boolean done;
+    private String name;
     private String message;
+    private ProblemType problemType;
+    private Technology technology;
+    private Region region;
 
-    public Task(boolean inWork, boolean done, String message) {
+
+    public Task(boolean inWork, boolean done, String name, String message, ProblemType problemType, Technology technology, Region region) {
         this.inWork = inWork;
         this.done = done;
+        this.name = name;
         this.message = message;
+        this.problemType = problemType;
+        this.technology = technology;
+        this.region = region;
     }
 
-    public boolean getInWork() {
+    public boolean isInWork() {
         return inWork;
     }
 
-    public boolean getDone() {
+    public boolean isDone() {
         return done;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ProblemType getProblemType() {
+        return problemType;
+    }
+
+    public Technology getTechnology() {
+        return technology;
+    }
+
+    public Region getRegion() {
+        return region;
     }
 
     public String getMessage() {
