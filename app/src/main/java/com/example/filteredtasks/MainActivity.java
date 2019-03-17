ad9f364b2aa026cbce1e45045e928035f7a213c6
sherruh <sherruh@gmail.com>
2019-03-17 21:30:32 +0600

    Connect to JSONDB!

--- app/src/main/java/com/example/filteredtasks/MainActivity.java
+++ app/src/main/java/com/example/filteredtasks/MainActivity.java
@@ -1,11 +1,33 @@
 package com.example.filteredtasks;
 
+import android.os.StrictMode;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.filteredtasks.serialyzers.JSonWorker;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        try {
+            JSonWorker jSonWorker=new JSonWorker();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/example/filteredtasks/serialyzers/JSonWorker.java
+++ app/src/main/java/com/example/filteredtasks/serialyzers/JSonWorker.java
@@ -1,11 +1,14 @@
 package com.example.filteredtasks.serialyzers;
 
 import android.util.Log;
+import android.webkit.WebResourceRequest;
 
 import com.example.filteredtasks.enums.ProblemType;
 import com.example.filteredtasks.enums.Region;
 import com.example.filteredtasks.enums.Technology;
 
+
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,9 +19,42 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+
+
 
 public class JSonWorker {
 
+    public JSonWorker() throws IOException, JSONException {
+        String url = "https://my-json-server.typicode.com/sherruh/DBJSonFilteredTasks/posts";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        // optional default is GET
+        //con.setRequestMethod("GET");
+        //add request header
+        //con.setRequestProperty("Content-Type", "text/plain; charset=utf-8");
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        System.out.println(response.toString());
+
+        JSONObject myResponse = new JSONObject(response.toString());
+        System.out.println("result after Reading JSON Response");
+        System.out.println("statusCode- "+myResponse.getString("id"));
+        System.out.println("statusMessage- "+myResponse.getString("title"));
+    }
 }
